c587569a0c966ec7fe5c16d2578f50b27053444c
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-24 14:25:50 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/ialmeida/workshopmongodb/domain/Post.java
+++ src/main/java/br/com/ialmeida/workshopmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package br.com.ialmeida.workshopmongodb.domain;
 
 import br.com.ialmeida.workshopmongodb.dto.AuthorDTO;
+import br.com.ialmeida.workshopmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -20,6 +23,7 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
