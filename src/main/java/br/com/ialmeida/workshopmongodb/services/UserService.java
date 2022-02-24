3d3b9d543dc1b1019931d0a5b2d4701f48705e8d
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-24 11:50:19 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/ialmeida/workshopmongodb/services/UserService.java
+++ src/main/java/br/com/ialmeida/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package br.com.ialmeida.workshopmongodb.services;
 
 import br.com.ialmeida.workshopmongodb.domain.User;
 import br.com.ialmeida.workshopmongodb.repositories.UserRepository;
+import br.com.ialmeida.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id " + id));
     }
 
 }
