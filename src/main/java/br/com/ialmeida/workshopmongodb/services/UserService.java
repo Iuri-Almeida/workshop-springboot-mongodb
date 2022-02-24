c98d262c80410d9bbd9dc2c6a0a8e482b041beb6
Iuri Almeida <ialmeida@vertigo.com.br>
2022-02-24 12:11:53 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/ialmeida/workshopmongodb/services/UserService.java
+++ src/main/java/br/com/ialmeida/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.ialmeida.workshopmongodb.services;
 
 import br.com.ialmeida.workshopmongodb.domain.User;
+import br.com.ialmeida.workshopmongodb.dto.UserDTO;
 import br.com.ialmeida.workshopmongodb.repositories.UserRepository;
 import br.com.ialmeida.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id " + id));
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 
 }
